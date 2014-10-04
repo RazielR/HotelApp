26109088ca2ab646349a89567c58e9ade6f61dbb
Raziel Rodriguez Zagal <razielrz00@hotmail.com>
2014-10-04 11:06:20 -0500

    Actions

---
+++ js/actions.js
@@ -0,0 +1,31 @@
+// actions.js
+var fn = {
+    init: function(){
+        document.addEventListener('deviceready',fn.device,false);
+      
+    },
+    device: function(){
+        $('#regSend').click(fn.registro);
+    },
+    registro: function(){
+        //Tomamos los campos y con el .val sacamos el contenido
+        var nombre = $('#regName').val();
+        var mail = $('#regMail').val();
+        var tel = $('#regTel').val();
+        if(nombre != '' && mail != '' && tel != ''){
+         // enviar datos al servidor
+        }else{
+            alert("Todos los campos son requeridos");
+        }
+        
+    }
+};
+
+//Esta esta en Java Script
+// window.addEventListener('load',fn.init,false);
+
+//Esta esta en Jquery
+$('document').ready(fn.init);
+
+//Este ejemeplo se toma de la documentacion de jquery
+// $(fn.init); //Es lo equivalente
